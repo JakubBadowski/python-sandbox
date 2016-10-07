83b6fad054bf3d6357139c1ecb874f97f44a66d7
JakubBadowski <jakub.badowski@gmail.com>
2016-10-07 12:07:18 +0200

    variance added

--- some_statistics.py
+++ some_statistics.py
@@ -1,15 +1,14 @@
 #! python3
 
-
 import statistics as st
-
 
 ex_list = [3,6,3,6,7,7,5,4,3,3,5,3,6,7,2,5,2,8,4]
 
 mean = st.mean(ex_list)         # średnia arytmenyczna
 median = st.median(ex_list)     # mediana 
 stdev = st.stdev(ex_list)       # odchylenie standardowe
+variance = st.variance(ex_list) # analiza wariancji
 
-print(mean, median, stdev)
+print(mean, median, stdev, variance)
 
 
