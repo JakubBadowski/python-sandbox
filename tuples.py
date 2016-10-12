5b78c669b062a5f89315c02244c32b9804624550
JakubBadowski <jakub.badowski@gmail.com>
2016-10-12 08:59:34 +0200

    Tuples added

---
+++ tuples.py
@@ -0,0 +1,19 @@
+#! python3
+
+print('Lists and Tuples')
+
+# Tuples
+tup1 = 1,2,67,3,6,4
+tup2 = (4,2,56,2,6)
+
+print(tup1)
+print(tup2)
+
+## example no. 1
+
+def example():
+    return 1,4
+
+x,y = example()
+print(x,y)
+
