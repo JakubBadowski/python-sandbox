f1cfcb85b45b56beb12a9b916a858ff2d2c12aaf
JakubBadowski <jakub.badowski@gmail.com>
2016-10-13 11:54:20 +0200

    some more tkint

--- tkinter/first.py
+++ tkinter/first.py
@@ -7,15 +7,38 @@ class Window(Frame):
         Frame.__init__(self, master)
 
         self.master = master
-
         self.init_window()
 
     def init_window(self):
         self.master.title("This is my first tkinter app")
         self.pack(fill=BOTH, expand=1)
 
-        quitButton = Button(self, text="Quit")
-        quitButton.place(x=10, y=10)
+        # menu
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        file = Menu(menu)
+        file.add_command(label='Save', command=self.client_samp)
+        file.add_command(label='Exit', command=self.client_exit)
+        menu.add_cascade(label='File', menu=file)
+
+        edit = Menu(menu)
+        edit.add_command(label='Undo', command=self.client_samp)
+        menu.add_cascade(label='Edit', menu=edit)
+        
+
+        # buttons
+        sampButton = Button(self, text="Sample button", command=self.client_samp)
+        sampButton.place(x=10, y=10)
+
+        quitButton = Button(self, text="Quit", command=self.client_exit)
+        quitButton.place(x=100, y=10)
+
+    def client_samp(self):
+        print('Click log...')
+
+    def client_exit(self):
+        exit()
 
 
 root = Tk()
